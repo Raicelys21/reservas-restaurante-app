3d9d1641a4727421fedaebd836769f8818d3269f
Raicelys Suero <raicelys21@hotmail.com>
2024-03-11 22:04:21 -0400

    fix error

--- mesa-selecta/src/App.jsx
+++ mesa-selecta/src/App.jsx
@@ -1,21 +1,20 @@
+import React from 'react'; // Make sure to import React
+
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-import './Components/Registro y Login/Login'
-import Reserva from './Components/Reserva/Reserva'
-import RegistroU from './Components/Registro y Login/RegistroU'
+import './Components/Registro y Login/Login';
+import Reserva from './Components/Reserva/Reserva';
+import RegistroU from './Components/Registro y Login/RegistroU';
 import RestaurantesRegistro from './Components/Restaurantes/RestaurantesRegistro';
-import ChooseUserType from './Components/Registro y Login/ChooseUserType'
-import 
-
+import ChooseUserType from './Components/Registro y Login/ChooseUserType';
 
 function App() {
-
   return (
     <>
-       <ChooseUserType/>
+      <ChooseUserType />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
